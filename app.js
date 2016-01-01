4c1be29d973d90bd9132329dbcd81f2dae774573
David McClure <dave@xerotrope.org>
2016-01-01 15:07:57 -0800

    make canvas full width and height

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-function createCanvas() {
-  return oCanvas.create({
+var canvas;
+
+function resizeCanvas(canvas) {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+window.onresize = function(event) {
+  resizeCanvas(canvas);
+}
+
+function runApp() {
+  canvas = oCanvas.create({
     canvas: "#canvas",
     background: "#0cc"
   });
+
+  resizeCanvas(canvas);
+
+  var circle = canvas.display.ellipse({
+    x: 0,
+    y: 0,
+    radius: 80,
+    fill: "#0aa"
+  });
+
+  for (i=0;i<20;i++) {
+    circle = circle.clone({
+      x: circle.x + 200,
+      y: circle.y + 200
+    });
+    canvas.addChild(circle);
+  }
 }
 
-oCanvas.domReady(createCanvas);
+oCanvas.domReady(runApp);
+
