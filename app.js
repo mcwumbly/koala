1a409fa3a91c23c4c922b173d373eb13deb08d56
David McClure <dave@xerotrope.org>
2016-01-01 21:16:13 -0800

    scale objects on canvas to virtual normalized 1000px x 1000px grid

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 var canvas;
 
-function resizeCanvas(canvas) {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-
-window.onresize = function(event) {
-  resizeCanvas(canvas);
-}
-
 function runApp() {
   canvas = oCanvas.create({
     canvas: "#canvas",
     background: "#0cc"
   });
-
-  resizeCanvas(canvas);
 
   var circle = canvas.display.ellipse({
     x: 0,
@@ -27,10 +16,44 @@ function runApp() {
   for (i=0;i<20;i++) {
     circle = circle.clone({
       x: circle.x + 200,
-      y: circle.y + 200
+      y: circle.y + 200,
     });
+    setOriginalCircleDimensions(circle);
     canvas.addChild(circle);
   }
+  
+  resizeCanvas(canvas);
+}
+
+function setOriginalCircleDimensions(circle) {
+  circle.x0 = circle.x;
+  circle.y0 = circle.y;
+  circle.radius0 = circle.radius;
+}
+
+function scaleCircle(circle, scaleFactor) {
+  circle.x = circle.x0 * scaleFactor;
+  circle.y = circle.y0 * scaleFactor;
+  circle.radius = circle.radius0 * scaleFactor;
+}
+
+function resizeCanvas(canvas) {
+  var normalizeLength = 1000;
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var shortSide = width < height ? width : height;
+  var scaleFactor = shortSide / normalizeLength;
+
+  _.forEach(canvas.children, function(child) {
+    scaleCircle(child, scaleFactor);
+  });
+
+  canvas.width = width;
+  canvas.height = height;
+}
+
+window.onresize = function(event) {
+  resizeCanvas(canvas);
 }
 
 oCanvas.domReady(runApp);
